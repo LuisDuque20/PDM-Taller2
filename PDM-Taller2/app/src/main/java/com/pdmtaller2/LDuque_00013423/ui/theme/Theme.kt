195aed8378ae39e9e1666df60140a62a50bd59ac
LuisDuque20 <00013423@uca.edu.sv>
2025-05-01 21:12:39 -0600

    Subiendo correccion con scaffold

--- PDM-Taller2/app/src/main/java/com/pdmtaller2/LDuque_00013423/ui/theme/Theme.kt
+++ PDM-Taller2/app/src/main/java/com/pdmtaller2/LDuque_00013423/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.pdmtaller2.LDuque_00013423.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -22,12 +21,12 @@ private val LightColorScheme = lightColorScheme(
     secondary = PurpleGrey40,
     tertiary = Pink40
 
-    /* Other default colors to override
+    /* Other default theme colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
+    onSecondary = Color.Black,
+    onTertiary = Color.Black,
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
     */
@@ -36,16 +35,14 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun FoodSpotByLDuqueTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true, // Esta línea ya declara dynamicColor
     content: @Composable () -> Unit
-) {    dynamicColor: Boolean = true,
-
+) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
--- PDM-Taller2/app/src/main/java/screens/MainScreen.kt
+++ PDM-Taller2/app/src/main/java/screens/MainScreen.kt
@@ -15,72 +15,109 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
-import com.pdmtaller2.LDuque_00013423.restaurants
-import com.pdmtaller2.LDuque_00013423.ui.theme.FoodSpotByLDuqueTheme
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.pdmtaller2.LDuque_00013423.restaurants
+import com.pdmtaller2.LDuque_00013423.ui.theme.FoodSpotByLDuqueTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(navController: NavController) {
     val modifier = Modifier
-    Column(
-        modifier = modifier.fillMaxSize()
-            .background(Color.DarkGray)
-            .padding(20.dp),
-        horizontalAlignment = Alignment.Start,
-        verticalArrangement = Arrangement.Top
-    ) {
-
-        val categories = listOf("Comida mexicana", "Comida rapida", "Postres")
-        categories.forEach { category ->
-            Column {
-                Text(text = category, color = Color.White)
-                LazyRow(
-                    modifier = modifier.fillMaxWidth()
-                        .height(200.dp)
-                        .border(width = 2.dp, color = Color.White, shape = RoundedCornerShape(10)).padding(5.dp)
-                ) {
-                    val filteredRestaurants = restaurants.filter { it.categories.contains(category) }
-                    items(filteredRestaurants.size) { index ->
-                        val restaurant = filteredRestaurants[index]
-                        Column(
-                            modifier = Modifier
-                                .padding(10.dp)
-                                .fillMaxHeight()
-                                .width(150.dp)
-                                .wrapContentWidth()
-                                .border(width = 1.dp, color = Color.White, shape = RoundedCornerShape(10)),
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            verticalArrangement = Arrangement.Center
-                        ) {
-                            Text(text = restaurant.name, color = Color.White)
-                            Text(text = restaurant.description, color = Color.Gray)
-                        }
-                    }
-                }
-                Spacer(modifier.height(10.dp))
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        "FoodSpot",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = Color.DarkGray,
+                    titleContentColor = Color.White
+                )
+            )
+        },bottomBar = {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Button(
+                    onClick = { navController.navigate("MainScreen") },
+                    modifier = Modifier.weight(1f)
+                ) { Icon(imageVector = Icons.Filled.Home, contentDescription = "Pantalla principal") }
+                Button(
+                    onClick = { navController.navigate("RestaurantScreen") },
+                    modifier = Modifier.weight(1f)
+                ) { Icon(imageVector = Icons.Filled.Star, contentDescription = "Restaurantes") }
+                Button(
+                    onClick = { navController.navigate("MyOrders") },
+                    modifier = Modifier.weight(1f)
+                ) { Icon(imageVector = Icons.Filled.ShoppingCart, contentDescription = "Mis órdenes") }
             }
         }
-        Row (modifier.fillMaxWidth()){
-            Button(onClick = {navController.navigate("MainScreen")}, modifier.weight(1f)) { Icon(imageVector = Icons.Filled.Home, contentDescription = "Pantalla principal")}
-            Button(onClick = {navController.navigate("RestaurantScreen")},modifier.weight(1f)) { Icon(imageVector = Icons.Filled.Star, contentDescription = "Restaurantes") }
-            Button(onClick = {navController.navigate("MyOrders")},modifier.weight(1f)) { Icon(imageVector = Icons.Filled.ShoppingCart, contentDescription = "Restaurantes") }
+    ) { paddingValues ->
+        Column(
+            modifier
+                .fillMaxSize()
+                .background(Color.DarkGray)
+                .padding(paddingValues)
+                .padding(horizontal = 20.dp),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Top
+        ) {
+            val categories = listOf("Comida mexicana", "Comida rapida", "Postres")
+            categories.forEach { category ->
+                Column {
+                    Text(text = category, color = Color.White)
+                    LazyRow(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(200.dp)
+                            .border(width = 2.dp, color = Color.White, shape = RoundedCornerShape(10))
+                            .padding(5.dp)
+                    ) {
+                        val filteredRestaurants = restaurants.filter { it.categories.contains(category) }
+                        items(filteredRestaurants.size) { index ->
+                            val restaurant = filteredRestaurants[index]
+                            Column(
+                                modifier = Modifier
+                                    .padding(10.dp)
+                                    .fillMaxHeight()
+                                    .width(150.dp)
+                                    .wrapContentWidth()
+                                    .border(
+                                        width = 1.dp,
+                                        color = Color.White,
+                                        shape = RoundedCornerShape(10)
+                                    ),
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.Center
+                            ) {
+                                Text(text = restaurant.name, color = Color.White)
+                            }
+                        }
+                    }
+                    Spacer(modifier.height(10.dp))
+                }
+            }
         }
     }
 }
